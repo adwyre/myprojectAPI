271216d075a7b8cf27a49eebec027cdfa4108cf1
Allison D <aadwyre@gmail.com>
2023-06-19 14:59:46 -0500

    create endpoints

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,5 +1,7 @@
 using MyProjectAPI.Data;
 using Microsoft.AspNetCore.Mvc;
+using MyProjectAPI.Models;
+using MyProjectAPI.Dtos;
 
 namespace MyProjectAPI.Controllers;
 
@@ -13,9 +15,95 @@ public class ProjectController : ControllerBase
         _dapper = new DataContextDapper(config);
     }
     
-    [HttpGet("TestConnection")]
-    public DateTime TestConnection()
+    [HttpGet("GetAllProjects")]
+    public IEnumerable<Project> GetAllProjects()
     {
-        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+        string sql = "SELECT * FROM ProjectManagerSchema.Projects";
+        IEnumerable<Project> projects = _dapper.LoadData<Project>(sql);
+        return projects;
+
     }
+
+    [HttpGet("GetProject/{Id}")]
+    public Project GetProject(int Id)
+    {
+        string sql = "SELECT * FROM ProjectManagerSchema.Projects WHERE Id = " + Id.ToString(); 
+        Project project = _dapper.LoadDataSingle<Project>(sql);
+        return project;
+    }
+    
+    [HttpPut("EditProject/{Id}")]
+    public IActionResult EditProject(Project project)
+    {
+        string sql = @"
+        UPDATE ProjectManagerSchema.Projects
+            SET [Title] = '" + project.Title + 
+                "', [Tag] = '" + project.Tag +
+                "', [Description] = '" + project.Description + 
+                "', [Status] = '" + project.Status + 
+                "', [Priority] = '" + project.Priority + 
+                "', [StartDate] = '" + project.StartDate + 
+                "', [DueDate] = '" + project.DueDate + 
+                "', [Completed] = '" + project.Completed + 
+            "' WHERE Id = " + project.Id;
+        
+        Console.WriteLine(sql);
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        } 
+
+        throw new Exception("Failed to Update Project");
+    }
+
+
+    [HttpPost("AddProject")]
+    public IActionResult AddProject(ProjectToAdd project)
+    {
+        string sql = @"INSERT INTO ProjectManagerSchema.Projects(
+                [Title],
+                [Tag],
+                [Description],
+                [Status],
+                [Priority],
+                [StartDate],
+                [DueDate],
+                [Completed]
+            ) VALUES (" +
+                "'" + project.Title + 
+                "', '" + project.Tag +
+                "', '" + project.Description + 
+                "', '" + project.Status + 
+                "', '" + project.Priority + 
+                "', '" + project.StartDate + 
+                "', '" + project.DueDate + 
+                "', '" + project.Completed + 
+            "')";
+        
+        Console.WriteLine(sql);
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        } 
+
+        throw new Exception("Failed to Add Project");
+    }
+
+    [HttpDelete("DeleteProject/{Id}")]
+    public IActionResult DeleteProject(int Id)
+    {
+        string sql = "DELETE FROM ProjectManagerSchema.Projects WHERE Id = " + Id.ToString();
+        
+        Console.WriteLine(sql);
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        } 
+
+        throw new Exception("Failed to Delete Project");
+    }
+    
 }
